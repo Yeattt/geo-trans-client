55cd62b45d49a745a494bbecebb7e683f6c7665d
Yeattt <nekz.8896@gmail.com>
2023-05-25 18:33:11 -0500

    refactor: :art: code cleaned

--- src/pages/admin/vehicles/VehiclesHome.jsx
+++ src/pages/admin/vehicles/VehiclesHome.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { AdminLayout } from "../../../components/layouts";
+
+import { AdminLayout } from '../../../components/layouts';
+import { VehiclesCard } from '../../../components';
 
 export const VehiclesHome = () => {
    const vehicles = [
@@ -17,66 +19,22 @@ export const VehiclesHome = () => {
          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-2xl text-white font-bold my-4">Vehículos</h2>
             <div className="overflow-hidden shadow-sm sm:rounded-lg">
+               <Link to="/admin/vehicles/create">
+                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                     Añadir Nuevo
+                  </button>
+               </Link>
+
                <div className="p-6">
                   {vehicles.length > 0 ? (
                      <div className="grid grid-cols-1 gap-4">
                         {vehicles.map((vehicle) => (
-                           <div
-                              key={vehicle.id}
-                              className="flex items-center justify-between bg-gray-700 p-4 rounded-md"
-                           >
-                              <div className="flex-1">
-                                 <div className="text-white font-bold">ID:</div>
-                                 <div className="text-white">{vehicle.id}</div>
-                              </div>
-                              <div className="flex-1">
-                                 <div className="text-white font-bold">Marca:</div>
-                                 <div className="text-white">{vehicle.make}</div>
-                              </div>
-                              <div className="flex-1">
-                                 <div className="text-white font-bold">Modelo:</div>
-                                 <div className="text-white">{vehicle.model}</div>
-                              </div>
-                              <div className="flex-1">
-                                 <div className="text-white font-bold">Año:</div>
-                                 <div className="text-white">{vehicle.year}</div>
-                              </div>
-                              <div className="flex-1">
-                                 <div className="text-white font-bold">Color:</div>
-                                 <div className="text-white">{vehicle.color}</div>
-                              </div>
-                              <div>
-                                 {/* Agrega aquí los botones de editar, eliminar y ver */}
-                                 <Link to={`/admin/vehicles/update/${vehicle.id}`}>
-                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
-                                       Editar
-                                    </button>
-                                 </Link>
-
-                                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2">
-                                    Eliminar
-                                 </button>
-
-                                 <Link to={`/admin/vehicles/${vehicle.id}`}>
-                                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                       Ver
-                                    </button>
-                                 </Link>
-                              </div>
-                           </div>
+                           <VehiclesCard key={vehicle.id} vehicle={vehicle} />
                         ))}
                      </div>
                   ) : (
                      <p className="text-gray-500">No hay vehículos disponibles.</p>
                   )}
-
-                  <br />
-
-                  <Link to="/admin/vehicles/create">
-                     <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                        Añadir Nuevo
-                     </button>
-                  </Link>
                </div>
             </div>
          </div>
