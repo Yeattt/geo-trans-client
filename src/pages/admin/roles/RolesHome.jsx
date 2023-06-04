24609337426f26e27e7aae8b2d7ac08ab7a0982d
Yeattt <nekz.8896@gmail.com>
2023-06-04 17:26:41 -0500

    refactor: :lipstick: roles admin ui updated

--- src/pages/admin/roles/RolesHome.jsx
+++ src/pages/admin/roles/RolesHome.jsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
+
+import { FaUserCog } from 'react-icons/fa';
+import { MdExitToApp } from 'react-icons/md';
+import { BiSearchAlt } from 'react-icons/bi';
+
 import { AdminLayout } from '../../../components/layouts';
-import { RolesCard } from '../../../components/admin';
+import { RolesCard, CreateFormModal } from '../../../components';
 import { useGetApiData } from '../../../hooks';
 
 export const RolesHome = () => {
    const { isLoading, data } = useGetApiData('/roles');
    const [roles, setRoles] = useState([]);
+   const [isCreateModalActive, setIsCreateModalActive] = useState(false);
 
    useEffect(() => {
       if (!isLoading) {
@@ -15,27 +21,148 @@ export const RolesHome = () => {
       }
    }, [isLoading, data]);
 
+   const handleIsCreateModalActive = (status) => {
+      setIsCreateModalActive(status);
+   }
+
    return (
       <AdminLayout>
-         <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 className="text-2xl text-white font-bold my-4">Roles</h2>
-            <div className="overflow-hidden shadow-sm sm:rounded-lg">
-               <Link to="/admin/roles/create">
-                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                     Añadir Nuevo
-                  </button>
-               </Link>
+         <div className="min-w-full bg-gray-200">
+            <div className="bg-white min-w-full flex items-center justify-between px-6 py-1">
+               <div className="flex flex-col justify-between px-6 py-1">
+                  <span className="text-2xl text-purplePz font-bold">Roles</span>
 
-               <div className="p-6">
-                  {roles.length > 0 ? (
-                     <div className="grid grid-cols-1 gap-4">
-                        {roles.map((role) => (
-                           <RolesCard key={role.id} role={role} />
-                        ))}
+                  <span className="text-xs font-bold">{`Admin > Roles`}</span>
+               </div>
+
+               <div className="flex items-center justify-center">
+                  <span className="font-bold cursor-pointer">Jefferson</span>
+
+                  <div className="bg-purplePz w-12 h-12 flex items-center justify-center rounded-full cursor-pointer ml-3">
+                     <img
+                        className="object-cover w-[95%] h-[95%] rounded-full"
+                        src="https://i1.sndcdn.com/artworks-pfkZ3eJZ5aIGjxDP-lvIbog-t500x500.jpg"
+                     />
+                  </div>
+
+                  <span className="ml-3 text-2xl text-red-600 font-bold cursor-pointer">
+                     <MdExitToApp />
+                  </span>
+               </div>
+            </div>
+
+            <br />
+
+            <div className="flex items-center justify-around">
+               <div className="bg-gradient-to-r from-secondary to-secondaryHover rounded-md h-32 w-[20%] flex flex-col items-center justify-center px-6 py-4">
+                  <div className="flex flex-row justify-center items-center">
+                     <div className="text-xl flex items-center justify-center">
+                        <span className="border border-gray-300 bg-white w-16 h-16 rounded-full flex justify-center items-center mr-6">
+                           <FaUserCog className="text-3xl text-secondary" />
+                        </span>
+
+                        <div className="flex flex-col justify-center">
+                           <span className="font-bold text-white">{roles.length}</span>
+                           <span className="text-sm text-white font-bold">Roles registrados</span>
+                        </div>
                      </div>
-                  ) : (
-                     <p className="text-gray-500">No hay roles disponibles.</p>
-                  )}
+                  </div>
+               </div>
+
+               <div className="bg-gradient-to-r from-orangePz to-orangePzHover rounded-md h-32 w-[50%] flex flex-col items-center justify-center px-6 py-4">
+                  <div className="flex flex-row justify-center items-center">
+                     <div className="text-xl flex items-center justify-center">
+                        <span className="border border-gray-300 bg-white w-16 h-16 rounded-full flex justify-center items-center mr-6">
+                           <FaUserCog className="text-3xl text-orangePz" />
+                        </span>
+
+                        <div className="flex flex-col justify-center">
+                           <span className="font-bold text-white">{roles.length}</span>
+                           <span className="text-sm text-white font-bold">Roles registrados</span>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               <div className="bg-gradient-to-r from-purplePz to-purplePzHover rounded-md h-32 w-[20%] flex flex-col items-center justify-center px-6 py-4">
+                  <div className="flex flex-row justify-center items-center">
+                     <div className="text-xl flex items-center justify-center">
+                        <span className="border border-gray-300 bg-white w-16 h-16 rounded-full flex justify-center items-center mr-6">
+                           <FaUserCog className="text-3xl text-purplePz" />
+                        </span>
+
+                        <div className="flex flex-col justify-center">
+                           <span className="font-bold text-white">{roles.length}</span>
+                           <span className="text-sm text-white font-bold">Roles registrados</span>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <br />
+
+            <div className="flex items-center justify-center">
+               <div className="bg-white rounded-sm w-[96.5%] flex flex-row items-center justify-between px-2 py-2">
+                  <div>
+                     Yeat
+                  </div>
+
+                  <div className="bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition w-72 h-9 flex items-center overflow-hidden">
+                     <input
+                        className="bg-transparent w-[87%] h-full px-2 pl-2 py-2 pb-3 text-base"
+                        type="text"
+                        placeholder="Buscar rol..."
+                     />
+
+                     <div className="bg-purplePz w-[13%] h-full border-l flex items-center justify-center cursor-pointer">
+                        <BiSearchAlt className="text-xl text-white" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            {/* // * IMPORTANTE: Prueba del modal para crear */}
+            {
+               isCreateModalActive && <CreateFormModal handleIsCreateModalActive={handleIsCreateModalActive} module="Roles" />
+            }
+
+
+            <br />
+
+            <div className="flex items-center justify-center">
+               <div className="bg-white rounded-sm w-[96.5%] flex flex-col justify-between px-2 py-2">
+                  <div className="h-14 w-full flex items-center justify-between px-3 py-10 mb-7 border-b">
+                     <span className="text-lg font-bold text-purplePz">Lista de Roles</span>
+
+                     <button
+                        className="bg-purplePz w-32 py-2 rounded-md font-bold text-white"
+                        onClick={() => handleIsCreateModalActive(true)}
+                     >
+                        Añadir
+                     </button>
+                  </div>
+
+                  <table className="text-sm">
+                     <thead>
+                        <tr>
+                           <th className="px-6 py-2 text-purplePz">ID</th>
+                           <th className="px-6 py-2 text-purplePz">Nombre</th>
+                           <th className="px-6 py-2 text-purplePz">Acciones</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        {
+                           roles.map(role => (
+                              <RolesCard key={role.id} role={role} />
+                           ))
+                        }
+                     </tbody>
+                  </table>
+
+                  <div className="flex items-center justify-center mt-5">
+                     Kankan
+                  </div>
                </div>
             </div>
          </div>
