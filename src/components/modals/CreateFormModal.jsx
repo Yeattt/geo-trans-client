117bcc9f2eb4783b6a908925dfe3bd7097817241
Yeattt <nekz.8896@gmail.com>
2023-06-03 20:57:13 -0500

    feat: :lipstick: create modals added

---
+++ src/components/modals/CreateFormModal.jsx
@@ -0,0 +1,26 @@
+import { AiOutlineClose } from 'react-icons/ai';
+
+import { VehiclesCreateForm } from '../admin';
+
+export const CreateFormModal = ({ handleIsCreateModalActive }) => {
+   return (
+      <div className="w-screen h-screen bg-black bg-opacity-50 absolute top-0 left-0 flex justify-center items-center overflow-hidden">
+         <div className="w-2/3 h-auto bg-purplePz rounded-md">
+            <div className="w-full h-[55px] rounded-md flex items-center justify-between px-2">
+               <h2 className="text-xl text-white font-semibold">Crear</h2>
+
+               <span 
+                  className="w-8 h-8 text-lg text-white bg-red-600 hover:bg-red-700 transition-all cursor-pointer rounded-full flex items-center justify-center"
+                  onClick={() => handleIsCreateModalActive(false)}
+               >
+                  <AiOutlineClose />
+               </span>
+            </div>
+            
+            <div className="w-full h-[calc(100% - 55px)] bg-white px-7 py-5">
+               <VehiclesCreateForm />
+            </div>
+         </div>
+      </div>
+   );
+}
