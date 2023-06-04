74757eebc438d88d1820494f168de863e12f4228
Yeattt <nekz.8896@gmail.com>
2023-06-04 17:42:39 -0500

    refactor: :safety_vest: clients create validations updated

--- src/components/admin/clients/ClientsCreateForm.jsx
+++ src/components/admin/clients/ClientsCreateForm.jsx
@@ -5,10 +5,18 @@ import { useCreateForm } from '../../../hooks';
 
 // * Yup es una librería que realiza y verifica las validaciones de los campos que se especifican
 const validationSchema = Yup.object().shape({
-   documento: Yup.number().max(11,'EL maximo es de 11 digitos').required('Campo requerido'),
-   nombre: Yup.string().required('Campo requerido'),
-   razonSocial: Yup.string().required('Campo requerido'),
-   telefono: Yup.number().max(9,'El maximo es de 9 digitos').required('Campo requerido')
+   documento:     Yup.number()
+                     .typeError('El documento debe ser un número')
+                     .required('Campo requerido')
+                     .test('len', 'Debe tener 11 dígitos', val => val && val.toString().length === 11),
+   nombre:        Yup.string()
+                     .required('Campo requerido'),
+   razonSocial:   Yup.string()
+                     .required('Campo requerido'),
+   telefono:      Yup.number()
+                     .typeError('El telefono debe ser un número')
+                     .required('Campo requerido')
+                     .test('len', 'Debe tener 9 dígitos', val => val && val.toString().length === 11),
 });
 
 export const ClientsCreateForm = () => {
@@ -17,7 +25,7 @@ export const ClientsCreateForm = () => {
       nombre: '',
       razonSocial: '',
       telefono: ''
-   },'clients');
+   }, 'clients');
 
    return (
       <Formik
@@ -26,62 +34,68 @@ export const ClientsCreateForm = () => {
          onSubmit={onSubmitForm}
       >
          <Form>
-            <div className="mb-4">
-               <label htmlFor="documento" className="text-black font-semibold block mb-2">
-                  Documento:
-               </label>
-               <Field
-                  type="text"
-                  id="documento"
-                  name="documento"
-                  className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
-               />
-               <ErrorMessage name="documento" component="div" className="text-red-500" />
+            <div className="grid grid-cols-2 gap-4">
+               <div className="mb-4">
+                  <label htmlFor="documento" className="text-black font-semibold block mb-2">
+                     Documento:
+                  </label>
+                  <Field
+                     type="text"
+                     id="documento"
+                     name="documento"
+                     className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
+                     placeholder="Vehículo..."
+                  />
+                  <ErrorMessage name="documento" component="div" className="text-red-500" />
+               </div>
+
+               <div className="mb-4">
+                  <label htmlFor="nombre" className="text-black font-semibold block mb-2">
+                     Nombre:
+                  </label>
+                  <Field
+                     type="text"
+                     id="nombre"
+                     name="nombre"
+                     className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
+                     placeholder="Vehículo..."
+                  />
+                  <ErrorMessage name="nombre" component="div" className="text-red-500" />
+               </div>
+
+               <div className="mb-4">
+                  <label htmlFor="razonSocial" className="text-black font-semibold block mb-2">
+                     Razon Social:
+                  </label>
+                  <Field
+                     type="text"
+                     id="razonSocial"
+                     name="razonSocial"
+                     className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
+                     placeholder="Vehículo..."
+                  />
+                  <ErrorMessage name="razonSocial" component="div" className="text-red-500" />
+               </div>
+
+               <div className="mb-4">
+                  <label htmlFor="telefono" className="text-black font-semibold block mb-2">
+                     Telefono:
+                  </label>
+                  <Field
+                     type="text"
+                     id="telefono"
+                     name="telefono"
+                     className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
+                     placeholder="Vehículo..."
+                  />
+                  <ErrorMessage name="telefono" component="div" className="text-red-500" />
+               </div>
             </div>
 
-            <div className="mb-4">
-               <label htmlFor="nombre" className="text-black font-semibold block mb-2">
-                  Nombre:
-               </label>
-               <Field
-                  type="text"
-                  id="nombre"
-                  name="nombre"
-                  className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
-               />
-               <ErrorMessage name="nombre" component="div" className="text-red-500" />
-            </div>
-
-            <div className="mb-4">
-               <label htmlFor="razonSocial" className="text-black font-semibold block mb-2">
-                  Razon Social:
-               </label>
-               <Field
-                  type="text"
-                  id="razonSocial"
-                  name="razonSocial"
-                  className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
-               />
-               <ErrorMessage name="razonSocial" component="div" className="text-red-500" />
-            </div>
-
-            <div className="mb-4">
-               <label htmlFor="telefono" className="text-black font-semibold block mb-2">
-                  Telefono:
-               </label>
-               <Field
-                  type="text"
-                  id="telefono"
-                  name="telefono"
-                  className="w-full px-3 py-2 rounded bg-gray-200 text-black border border-gray-300 focus-within:border-purplePzHover transition"
-               />
-               <ErrorMessage name="telefono" component="div" className="text-red-500" />
-            </div>
-            
-            <div className="flex justify-between">
+            <div className="text-center mt-2">
                <button
                   type="submit"
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                  className="bg-purplePz hover:bg-purplePzHover transition-all text-white font-semibold py-2 px-4 rounded"
                >
                   Registrar
                </button>
