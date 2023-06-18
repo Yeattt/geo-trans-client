4a40d0e4e0dc9f628a4230f2fa870e6fd5ca8d98
Yeattt <nekz.8896@gmail.com>
2023-06-18 12:38:49 -0500

    feat: :passport_control: routes protected with authentication module

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,17 +1,44 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { AdminRoutes } from './';
-import { HomePage } from '../pages/'
 import { AuthRoutes } from './auth';
+import { HomePage } from '../pages/'
+import { useAuthStore } from '../hooks';
 
 export const AppRouter = () => {
+   const { status, checkAuthToken } = useAuthStore();
+
+   useEffect(() => {
+      checkAuthToken();
+   }, []);
+
+   if (status === 'checking') {
+      return (
+         <h3>Loading...</h3>
+      )
+   }
+
    return (
-      <>
-         <Routes>
-            <Route path="/admin/*" element={<AdminRoutes />} />
-            <Route path="/auth/*" element={<AuthRoutes />} />
-            <Route path="/" element={<HomePage />} />
-         </Routes> 
-      </>
+      <Routes>
+         {
+            (status === 'not-authenticated')
+               ? (
+                  <>
+                     <Route path="/auth/*" element={<AuthRoutes />} />
+                     <Route path="/*" element={<Navigate to="/auth/signin" />} />
+                  </>
+               )
+               : (
+                  <>
+                     <Route path="/admin/*" element={<AdminRoutes />} />
+                     <Route path="/*" element={<Navigate to="/admin/vehicles" />} />
+                  </>
+               )
+         }
+
+         <Route path="/" element={<HomePage />} />
+      </Routes>
    );
 }
