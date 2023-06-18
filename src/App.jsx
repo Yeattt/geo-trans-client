08424d4c234035f80694dfae6a03ada8d74d4ef6
Yeattt <nekz.8896@gmail.com>
2023-06-18 12:39:16 -0500

    feat: :sparkles: redux context added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './routes';
+
+import { store } from './store';
 
 export default function App() {
    return (
-      <>
+      <Provider store={store}>
          <AppRouter />
-      </>
+      </Provider>
    );
 }
