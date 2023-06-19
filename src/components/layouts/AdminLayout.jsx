f2cc46d7917042d91c6f9c27e17f9ea8b24cf6d3
Yeattt <nekz.8896@gmail.com>
2023-06-19 13:15:59 -0500

    refactor: :art: code of admin layout refactorized

--- src/components/layouts/AdminLayout.jsx
+++ src/components/layouts/AdminLayout.jsx
@@ -1,10 +1,8 @@
-import { AdimnNavbar, AdminSideMenu } from '../admin/';
+import { AdminSideMenu } from '../admin/';
 
 export const AdminLayout = ({ children }) => {
    return (
       <div className="flex flex-col h-screen">
-         {/* <AdimnNavbar /> */}
-
          <div className="flex flex-1">
             <AdminSideMenu />
 
