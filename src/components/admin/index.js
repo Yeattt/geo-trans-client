018b650471dd7b3eae7f289639d32adb613b0f6e
Yeattt <nekz.8896@gmail.com>
2023-06-19 13:14:51 -0500

    ci: :wrench: vehiclesType added to exports file

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -6,6 +6,7 @@ export * from './roles';
 export * from './trips';
 export * from './users';
 export * from './vehicles';
+export * from './vehiclesType';
 
 export * from './AdminSideMenu';
 export * from './AdminNavbar';
