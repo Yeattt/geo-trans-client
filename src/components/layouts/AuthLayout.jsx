b282bd20555dfc3e0cad9fd41de6432806ce530f
Yeattt <nekz.8896@gmail.com>
2023-06-21 21:54:56 -0500

    refactor: :lipstick: auth layout styles changed

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -3,32 +3,36 @@ import { Link } from 'react-router-dom';
 
 export const AuthLayout = ({ children }) => {
    return (
-      <div className="w-screen h-screen bg-gradient-to-br from-pinkPz to-purplePz flex items-center justify-center">
-         <div className="w-[70%] h-[75%] flex">
-            <div className="w-[60%] h-full p-20 bg-purplePz rounded-s-md flex flex-col items-center bg-[url('../assets/auth-banner.jpg')] bg-cover">
-               <div className="w-44 h-44 border-4 border-purplePz rounded-full overflow-hidden bg-white mb-10 flex items-center justify-center cursor-pointer">
-                  <Link to="/" className="w-full h-full flex items-center justify-center">
-                     <span className="text-8xl text-purplePz rounded-full">
-                        <IoSubway />
-                     </span>
+      <div className="w-screen h-screen bg-gray-300 flex items-center justify-center overflow-hidden">
+         <div className="w-[70%] h-[75%] flex relative">
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+               <div className="w-80 h-80 border-8 border-blue-900 rounded-full bg-primary absolute -top-44 -left-96"></div>
+               <div className="w-80 h-80 border-8 border-blue-900 rounded-full bg-primary absolute -bottom-44 -right-96"></div>
+            </div>
+
+            <div className="relative z-10 w-[50%] h-full p-20 bg-primary rounded-s-md flex flex-col items-center bg-[url('../assets/aut.jpg')] bg-cover">
+               <div className="relative z-10 w-44 h-44 border-4 border-primaryHover rounded-full overflow-hidden bg-white mb-10 flex items-center justify-center">
+                  <Link to="/" className="relative z-10 w-full h-full flex items-center justify-center">
+                     <IoSubway className="text-8xl text-primaryHover rounded-full" />
                   </Link>
                </div>
 
+               <div className="relative z-10">
+                  <h2 className="text-white text-4xl font-bold mb-10">Bienvenido a GeoTransporte!</h2>
 
-               <h2 className="text-white text-4xl font-bold mb-10">Bienvenido a GeoTransporte!</h2>
+                  <p className="text-white font-semibold">
+                     Recuerda que si aún no estás registrado, puedes hacerlo dando click en "Registrar Cuenta",
+                     lo cuál te redireccionará a la página con el formulario de registro.
 
-               <p className="text-white font-semibold">
-                  Recuerda que si aún no estás registrado, puedes hacerlo dando click en "Registrar Cuenta",
-                  lo cuál te redireccionará a la página con el formulario de registro.
+                     <br />
+                     <br />
 
-                  <br />
-                  <br />
-
-                  Una vez registrado debes esperar a ser aceptado para que tu cuenta entre en funcionamiento.
-               </p>
+                     Una vez registrado debes esperar a ser aceptado para que tu cuenta entre en funcionamiento.
+                  </p>
+               </div>
             </div>
 
-            <div className="w-[40%] h-full py-10 bg-white rounded-e-md">
+            <div className="w-[50%] h-full py-10 bg-white rounded-e-md z-10">
                {children}
             </div>
          </div>
