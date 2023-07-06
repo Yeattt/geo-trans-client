df92a2a78d3fcdefb6b97e8391758babee3f1c03
Yeattt <nekz.8896@gmail.com>
2023-07-06 17:32:20 -0500

    refactor: :truck: routes code and files refactorized

--- src/routes/admin/AdminRoutes.jsx
+++ src/routes/admin/AdminRoutes.jsx
@@ -1,20 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { ClientsRoutes, CompaniesRoutes, PermissionsRoutes, QuotesRoutes, RolesRoutes, TripsRoutes, UsersRoutes, VehiclesRoutes, VehiclesTypeRoutes, CalendarRoutes  } from './';
+import { UsersRoutes } from './UsersRoutes';
+import { CalendarPage, ClientsHome, CompaniesHome, PermissionsHome, QuotesHome, RolesHome, TripsHome, VehiclesHome, VehiclesTypeHome } from '../../pages/admin';
 
 export const AdminRoutes = () => {
    return (
       <Routes>
-         <Route path="/calendar/*" element={<CalendarRoutes />} />
-         <Route path="/clients/*" element={<ClientsRoutes />} />
-         <Route path="/companies/*" element={<CompaniesRoutes />} />
-         <Route path="/permissions/*" element={<PermissionsRoutes />} />
-         <Route path="/quotes/*" element={<QuotesRoutes />} />
-         <Route path="/roles/*" element={<RolesRoutes />} />
-         <Route path="/trips/*" element={<TripsRoutes />} />
+         <Route path="/calendar" element={<CalendarPage />} />
+         <Route path="/clients" element={<ClientsHome />} />
+         <Route path="/companies" element={<CompaniesHome />} />
+         <Route path="/permissions" element={<PermissionsHome />} />
+         <Route path="/quotes" element={<QuotesHome />} />
+         <Route path="/roles" element={<RolesHome />} />
+         <Route path="/trips" element={<TripsHome />} />
          <Route path="/users/*" element={<UsersRoutes />} />
-         <Route path="/vehicles/*" element={<VehiclesRoutes />} />
-         <Route path="/trucks/types/*" element={<VehiclesTypeRoutes />} />
+         <Route path="/vehicles" element={<VehiclesHome />} />
+         <Route path="/trucks/types" element={<VehiclesTypeHome />} />
       </Routes>
    );
 }
